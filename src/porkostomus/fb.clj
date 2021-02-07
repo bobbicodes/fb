a293794851a7d44549f5ae3f42ccf22ccc432ef5
porkostomus <porkostomus@mail.com>
2021-02-07 15:50:09 -0800

    retrieve by datetime

--- src/porkostomus/fb.clj
+++ src/porkostomus/fb.clj
@@ -1,6 +1,7 @@
 (ns porkostomus.fb
   (:require
    [cheshire.core :as json]
+   [clj-time.core :as t]
    [clj-time.coerce :as c]
    [clj-time.format :as f]))
 
@@ -10,7 +11,11 @@
   (filter #(< from (:timestamp %) to) (json/parse-string (slurp "resources/posts.json") true)))
 
 (comment
-  (f/unparse (f/formatters :rfc822)
+  (f/show-formatters)
+  (c/to-epoch (t/date-time 1986 10 14))
+  (f/unparse (f/formatters :year-month-day)
              (c/from-epoch 1195605098))
   (posts 1195244348 1195605098)
+  (posts (c/to-epoch (t/date-time 2010 10))
+         (c/to-epoch (t/date-time 2010 11)))
   )
