75f90929e3de460d1e9ad3883fbc7f7b25291a16
porkostomus <porkostomus@mail.com>
2021-02-07 14:05:17 -0800

    use clj-time

--- src/porkostomus/fb.clj
+++ src/porkostomus/fb.clj
@@ -1,6 +1,8 @@
 (ns porkostomus.fb
   (:require
-   [cheshire.core :as json]))
+   [cheshire.core :as json]
+   [clj-time.coerce :as c]
+   [clj-time.format :as f]))
 
 (defn posts
   "Retrieves all posts between 2 timestamps."
@@ -8,5 +10,7 @@
   (filter #(< from (:timestamp %) to) (json/parse-string (slurp "resources/posts.json") true)))
 
 (comment
+  (f/unparse (f/formatters :rfc822)
+             (c/from-epoch 1195605098))
   (posts 1195244348 1195605098)
   )
